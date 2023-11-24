b35638e73c56d97b62fffb3ec7bd8ab2c94833d7
SolipsesDualboot <w0477439.nscc.ca>
2023-11-24 11:58:16 -0400

    Version 0.01

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sol_lib as sol
 
-
 def main():
     selected_file = sol.select_file()
+    print(selected_file.read())
     sol.debugger(selected_file[0], "r")
 #    if selected_file[2] == "-c":
 #        file_out = sol.compile(selected_file)
