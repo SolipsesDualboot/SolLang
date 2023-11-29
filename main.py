b89d42e9038d9d4f1f27c1987b94d0b3c34c3178
SolipsesDualboot <w0477439.nscc.ca>
2023-11-29 10:30:58 -0400

    Version 0.02

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import sol_lib as sol
-
+print('{{to_print}}'.format(to_print = 2))
 def main():
-    selected_file = sol.select_file()
-    print(selected_file.read())
-    sol.debugger(selected_file[0], "r")
+    sol_selected_file = sol.select_file()
+    sol.debugger(sol_selected_file, "p")
 #    if selected_file[2] == "-c":
 #        file_out = sol.compile(selected_file)
 #    elif selected_file[2] == "-i":
-#        file_out = sol.interpret(selected_file)
-#    exec(file_out)
+    sol_file_out = sol.interpret(sol_selected_file)
+    exec(sol_file_out)
 
 
 if __name__ == "__main__":
--- sol_lib.py
+++ sol_lib.py
@@ -5,7 +5,7 @@
 def select_file():
     file = input("Select a file by name (No extension) ").split(" ")
     try:
-        file = open(file[0] + ".sol")
+        file = open(file[0])
     except FileNotFoundError: # If file is not found, exit
         print("File not found:", "\"{filename}\"".format(filename = file[0]))
         exit()
@@ -25,38 +25,45 @@ def raise_error(error, line):
 
 
 ## DEBUGGER - FIRST PASS
-def debug_start(file, mode):
+def debug_start(line, mode):
     indents = 0
     linenum = 0
-    for line in (file):
-        if line.isspace():
-            continue
-        if line[0] == "/":
-            continue
-#        if line[0] == " ":
-#            raise_error(2, linenum)
+    if line.isspace():
+        return
+    if line[0] == "/":
+        return
+    for i in range(len(line)):
+        if line[0] == " ":
+            
+            raise_error(2, linenum)
 #    for i in range(4):
 #        if line[i] == " ":
 #            indents += 1
-#    if indents != 4:
-#        error = 2
-        if mode == "p":
-            print(line)
+    if indents != 4:
+        error = 2
+    if mode == "p":
+        print(line)
 
 
 ## DEBUGGER - INVOKER
 def debugger(file, mode):
     for line in (file):
-        debug_start(file, "r")
+        debug_start(line, mode)
 
 
 ### CODE OUTPUTTING
 ## INTERPRETER
-def interpret():
-    pass
+def interpret(file):
+    code = ""
+    code = code + "print(\"hello world\")\n"
+    code = code + "print(\"{to_print}\".format(to_print = 2))\n"
+    return code
 
 
 ## COMPILER
 def compile(file):
     code = ""
+    code += "this = 2\n"
+    code += "print(\"hello world\")\n"
+    code += "print(\"{to_print}\".format(to_print = sol_file_out.this))\n"
     return code
